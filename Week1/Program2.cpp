c0d1c5fe7ac1c6c304b130f1b415e5a7bf4f43e0
25A31A05IA <narasimhareddy9121736498@gmail.com>
2026-02-21 15:46:32 +0530

    Program2.cpp

--- Week1/Program2.cpp
+++ Week1/Program2.cpp
@@ -1,1 +1,13 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  cout<<"Enter a&b value";
+  cin>>a>>b;
+  cout<<"Value of a is:"<<a<<endl<<"Value of b is:"<<b;
+  a = a+b;
+  b = a-b;
+  a = a-b;
+  cout<<"Value of a is:"<<a<<endl<<"Value of b is:"<<b;
+}
